eaa4dcc8713001890cb349926810838d2813fe07
Jim H Chang (RD-TW) <jim_h_chang@trend.com.tw>
2016-10-09 16:07:43 +0800

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ var edges = require('./csv/social_network.json')
 var cpnNodes = []
 var cpnEdges = []
 var id = []
-var limit = 50
+var limit = 400
+
+
+var stringEdges = JSON.stringify(edges)
+
 
 function dataPreProcess(){
 	for(var i=0; i<limit; i++){
@@ -16,17 +20,63 @@ function dataPreProcess(){
 	}
 	console.log(cpnNodes)
 	console.log(id)
-	for(var j=0; j<limit; j++){
+	for(var j=0; j<edges.length; j++){
 		// console.log(edges[j])
 		if(id.indexOf(edges[j].from) > 0 || id.indexOf(edges[j].to) >0){
 			cpnEdges.push(edges[j])
 		}
 	}
-	console.log(cpnEdges)
+	
+}
+var winner = []
+var winner_network = [] 
+function dump(){
+	var geek = require('./geek.json')
+	var geek_network = require('./geek_network.json') 
+	
+	for(var i=0;i<geek.length;i++){
+		var duplicate = geek[i]
+		duplicate.id += geek.length 
+		duplicate.label = 'somedody'
+		winner.push(duplicate)
+	}
+
+	for(var j=0;j<geek_network.length;j++){
+		var dup = geek_network[j]
+		dup.from += geek.length 
+		dup.to += geek.length 
+		winner_network.push(dup)
+	}
 }
 
-dataPreProcess()
+// dataPreProcess()
+// console.log('compnent nodes number = ' + cpnNodes.length)
+// console.log('component edges number = ' + cpnEdges.length)
+// fs.writeFile('geek.json',JSON.stringify(cpnNodes), (err) => {
+//  		if (err) throw err;
+//  		console.log('It\'s saved!');
+// 	})
+// fs.writeFile('geek_network.json',JSON.stringify(cpnEdges), (err) => {
+//  		if (err) throw err;
+//  		console.log('It\'s saved!');
+// 	})
 
+// dump()
+// fs.writeFile('winner.json',JSON.stringify(winner), (err) => {
+//  		if (err) throw err;
+//  		console.log('It\'s saved!');
+// 	})
+// fs.writeFile('winner_network.json',JSON.stringify(winner_network), (err) => {
+//  		if (err) throw err;
+//  		console.log('It\'s saved!');
+// 	})
+var geek = require('./geek.json')
+var geek_network = require('./geek_network.json')
+var winner = require('./winner.json')
+var winner_network = require('./winner_network.json')
+
+var totalNodes = geek.concat(winner)
+var totalEdges = geek_network.concat(winner_network)
 app.use(express.static('public'))
 
 app.use('/static', express.static('public'))
@@ -41,7 +91,7 @@ app.get('/about',function(req,res){
 
 app.get('/graph',function(req,res){
 	res.setHeader('Content-Type', 'application/json')
-	res.send(JSON.stringify({nodes:cpnNodes,edges:cpnEdges}))
+	res.send(JSON.stringify({nodes:totalNodes,edges:totalEdges}))
 })
 
 app.get('/edges',function(req,res){
